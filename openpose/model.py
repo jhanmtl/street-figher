d2b817800cb9461a925d400210f4eb64bc5f9723
jay <jhanmtl@gmail.com>
2019-09-29 09:13:11 -0400

    model

--- openpose/model.py
+++ openpose/model.py
@@ -9,16 +9,26 @@ import time
 import cv2
 import argparse
 import numpy as np
+import copy
 
+k=0
+h=600
+w=800
+xadj=150
+yadj=30
+delay=0.25
 
-
+pB_safe = False #not safe because can be harmed
+pA_safe = False
 
 ap=argparse.ArgumentParser()
 ap.add_argument('img',type=str)
+ap.add_argument('scale',type=int)
 ap.add_argument('data',type=str)
 
 args=ap.parse_args()
 img_url=args.img
+scale=args.scale
 data_url=args.data
 
 numbering=False
@@ -26,25 +36,98 @@ numbering=False
 data_cap=utils.predictionGrabber(data_url).start()
 img_cap=utils.frameGrabber(img_url).start()
 
-time.sleep(5)
-original_img=img_cap.read()
-h,w,d=original_img.shape
-canvas=np.zeros((h,w,d))
+utils.start_screen(h,w,xadj,yadj,delay)
 
+original_img=img_cap.read()
+cv2.imshow('',original_img)
+cv2.waitKey(1)
+h,w,d=original_img.shape
+#print(h)
+#print(w)
+game_h=int(h*scale)
+game_w=int(w*scale)
+#print(game_h)
+#print(game_w)
+
+white=(1,1,1)
+yellow=(0,1,1)
+black=(0,0,0)
+red=(0,0,1)
+blue=(1,0,0)
+green=(0,1,0)
+
+
+bgcolor=black    
+canvas=np.zeros((game_h,game_w,d))
+
+for i in range(len(bgcolor)):
+    canvas[:,:,i]=bgcolor[i]
+
+gradius=int(h/4)
+hradius=int(h/2)
+thickness=int(h/3)
+
+p1_life=100
+p2_life=100
+
+status=1
 while True:
+    if p1_life==0 or p2_life==0:
+        h=600
+        w=800
+        xadj=150
+        yadj=30
+        delay=0.25
+        utils.start_screen(h,w,xadj,yadj,delay)
+        p1_life=100
+        p2_life=100
+#        first_time=False
     cv2.imshow('',canvas)
     info=data_cap.read()
+#    print(info)
     if info !='pass':
         persons=info.split('^')
         p1=persons[0]
         p2=persons[1]
-        
-        joint_vec1,h1,w1=utils.parse_info(p1)
-        joint_vec2,h2,w2=utils.parse_info(p2)
 
-        new_canvas=utils.draw_human(canvas,joint_vec1)
-        new_canvas=utils.draw_humans(new_canvas,joint_vec2)
-    cv2.imshow('',new_canvas)
+        joint_vec1=utils.parse_info2(p1,game_h,game_w)
+       
+        new_canvas,new_joint_vec1=utils.draw_human2(copy.deepcopy(canvas),joint_vec1,game_h,0.5,game_h-50,gradius,hradius,thickness,blue,white)
+        joint_vec2=utils.parse_info2(p2,game_h,game_w)
+        new_canvas,new_joint_vec2=utils.draw_human2(copy.deepcopy(new_canvas),joint_vec2,game_h,0.5,game_h-50,gradius,hradius,thickness,green,white)
+                
+        health,pA_safe,pB_safe=utils.collision_detection(new_joint_vec1, new_joint_vec2, pB_safe, pA_safe,hradius)
+#        print(health)
+#        health, pA_safe, pB_safe=utils.collision_detection(joint_vec1, joint_vec2, pB_safe, pA_safe,50)
+        p1_life+=health[0]*10
+        p2_life+=health[1]*10
+
+        if health[0]!=0:
+            if 0 in new_joint_vec1.keys():
+                head1=new_joint_vec1[0]
+                cv2.circle(new_canvas, (head1['x'],head1['y']), hradius , red, -1)
+
+        if health[1]!=0:
+            if 0 in new_joint_vec1.keys():
+                head2=new_joint_vec2[0]
+                cv2.circle(new_canvas, (head2['x'],head2['y']), hradius , red, -1)
+        
+#        print(new_joint_vec1)
+#        
+        new_canvas = cv2.flip(new_canvas, 1 )
+        new_canvas,msg=utils.update_hp(new_canvas, p1_life, p2_life)
+        if msg!=0:
+            cv2.putText(new_canvas, msg, (int(w/2), int(h/2)), cv2.FONT_HERSHEY_COMPLEX, 1, red, 5, cv2.LINE_AA)
+         
+        
+        if msg!=0:
+            while k<500:
+                cv2.imshow('',new_canvas)
+                cv2.waitKey(1)
+                k+=1
+            k=0
+        else:
+            cv2.imshow('',new_canvas)
     if cv2.waitKey(1) & 0xff == ord('q'):
         cv2.destroyAllWindows()
         break
--- openpose/utils.py
+++ openpose/utils.py
@@ -8,26 +8,108 @@ from threading import Thread
 import cv2
 import requests
 import numpy as np
+import math
+import time
+#from playsound import playsound
+
+# Return true if line segments AB and CD intersect
+def not_intersect(A,B,C,D):
+    return ccw(A,C,D) == ccw(B,C,D) and ccw(A,B,C) == ccw(A,B,D)
+
+def ccw(A,B,C):
+    return (C["y"]-A["y"]) * (B["x"]-A["x"]) > (B["y"]-A["y"]) * (C["x"]-A["x"])
+
+def collision_detection(pA, pB, pB_safe, pA_safe,radius):
+  health = [0,0]
+  #return an array for how many points to deduct from health bar
+  if 0 in pA.keys() and 4 in pA.keys() and 7 in pA.keys() and 0 in pB.keys() and 4 in pB.keys() and 7 in pB.keys():  
+
+      pA_head = pA[0]
+      pA_rf = pA[7]
+      pA_lf = pA[4]
+      pB_head = pB[0]
+      pB_rf = pB[7]
+      pB_lf = pB[4]
+#      pA_midbody = {"x": (pA[1]["x"] + pA[8]["x"])/2, "y": (pA[1]["y"] + pA[8]["y"])/2}
+#      pB_midbody = {"x": (pB[1]["x"] + pB[8]["x"])/2, "y": (pB[1]["y"] + pB[8]["y"])/2}
+      #before recording a collision, make sure that the hit isn't being blocked
+      #check that pA is not blocking person B
+#      blockA_la_lf = not_intersect(pA[3], pA[4], pB[3], pB[4])
+#      blockA_ra_lf = not_intersect(pA[6], pA[7], pB[3], pB[4])
+#      blockA_la_rf = not_intersect(pA[3], pA[4], pB[6], pB[7])
+#      blockA_ra_rf = not_intersect(pA[6], pA[7], pB[6], pB[7])
+#      
+      #person A hits B head
+      dist_pA_lfist_pB_head = math.hypot(pB_head["x"] - pA_lf["x"], pB_head["y"] - pA_lf["y"])
+      dist_pA_rfist_pB_head = math.hypot(pB_head["x"] - pA_rf["x"], pB_head["y"] - pA_rf["y"])
+#      print(dist_pA_lfist_pB_head)
+#      print(dist_pA_rfist_pB_head)
+      if (dist_pA_lfist_pB_head<radius or dist_pA_rfist_pB_head<radius) :
+        if pB_safe==False:  
+          health[1]=-1
+          pB_safe = True
+          print('aaaaaaaaaaaaaaaaaaaaaaaaaa')
+
+      else:
+        pB_safe = False
+        
+        
+#      #person B hits A head
+      dist_pB_lfist_pA_head = math.hypot(pA_head["x"] - pB_lf["x"], pA_head["y"] - pB_lf["y"])
+      dist_pB_rfist_pA_head = math.hypot(pA_head["x"] - pB_rf["x"], pA_head["y"] - pB_rf["y"])
+##      print(dist_pB_lfist_pA_head)
+##      print(dist_pB_rfist_pA_head)
+      if (dist_pB_lfist_pA_head<radius or dist_pB_rfist_pA_head<radius) :
+        if pA_safe==False:  
+          health[0]=-1
+          pA_safe = True
+          print('bbbbbbbbbbbbbbbbbbbbbbbb')
+#
+      else:
+        pA_safe = False
+#        
+#      #person A hits B midbody
+#      dist_pA_lfist_pB_midbody = math.hypot(pB_midbody["x"] - pA_lf["x"], pB_midbody["y"] - pA_lf["y"])
+#      dist_pA_rfist_pB_midbody = math.hypot(pB_midbody["x"] - pA_rf["x"], pB_midbody["y"] - pA_rf["y"])
+#      if (dist_pA_lfist_pB_midbody<radius or dist_pA_rfist_pB_midbody<radius) :
+#        if pB_safe==False and (blockA_la_lf or blockA_ra_lf or blockA_la_rf or blockA_ra_rf):  
+#          health[1]=-1
+#          pB_safe = True
+#      else:
+#        pB_safe = False
+#     
+#      #person B hits A midbody
+#      dist_pB_lfist_pA_midbody = math.hypot(pA_midbody["x"] - pB_lf["x"], pA_midbody["y"] - pB_lf["y"])
+#      dist_pB_rfist_pA_midbody = math.hypot(pA_midbody["x"] - pB_rf["x"], pA_midbody["y"] - pB_rf["y"])
+#      if (dist_pB_lfist_pA_midbody<radius or dist_pB_rfist_pA_midbody<radius) :
+#        if pA_safe==False and (blockA_la_lf or blockA_ra_lf or blockA_la_rf or blockA_ra_rf):  
+#          health[1]=-1
+#          pA_safe = True
+#      else:
+#        pA_safe = False
+#        
+#      
+      return health, pA_safe, pB_safe  
+  else:
+      return [0,0],False,False
 
 def parse_info(p):
     p=p.replace('<','')
     p=p.replace('>','')
     info=p.split('|')
-    img_dims=info[0].split(',')
-    h=int(img_dims[0])
-    w=int(img_dims[1])
-    #
-    del info[0]
+
     
     joint_info={}
     for piece in info:
         joint=piece.split(',')
         joint_id=int(joint[0])
-        joint_x=int(float(joint[1])*w)
-        joint_y=int(float(joint[2])*h)
+        joint_x=(float(joint[1]))
+        joint_y=(float(joint[2]))
         subdict={'x':joint_x,'y':joint_y}
         joint_info[joint_id]=subdict
-    return joint_info,h,w
+    return joint_info
+
+
 
 def parse_parts(incoming,original,canvas):
     original_h,original_w,_=original.shape
@@ -50,8 +132,86 @@ def parse_parts(incoming,original,canvas):
     
     return parts,scale
 
+def parse_info2(p,h,w):
+    p=p.replace('<','')
+    p=p.replace('>','')
+    info=p.split('|')
 
-def draw_human(img,parts,scale,numbering=False):
+    
+    joint_info={}
+    for piece in info:
+        joint=piece.split(',')
+        joint_id=int(joint[0])
+        joint_x=int((float(joint[1])*w))
+        joint_y=int((float(joint[2])*h))
+        subdict={'x':joint_x,'y':joint_y}
+        joint_info[joint_id]=subdict
+    return joint_info
+
+
+def draw_human2(img,parts,screen_height,human_ratio,base,gradius,hradius,thickness,line_color,gcolor,numbering=False):
+    connections=[(0,1),(1,2),(1,5),(1,8),(1,11),(2,3),(3,4),(5,6),(6,7),(8,9),(9,10),(11,12),(12,13)]
+    keys=list(parts.keys())
+    
+    
+    X=[]
+    Y=[]
+    for i in parts.keys():
+        if i<14:
+            joint=parts[i]
+            joint_x=int(joint['x'])
+            joint_y=int(joint['y'])
+            X.append(joint_x)
+            Y.append(joint_y)
+    original_height=max(Y)-min(Y)
+    target_height=screen_height*human_ratio
+    h_scale=target_height/original_height
+    
+    for i in parts.keys():
+        parts[i]['x']=int(parts[i]['x']*h_scale)
+        parts[i]['y']=int(parts[i]['y']*h_scale)
+    
+    X=[]
+    Y=[]
+    for i in parts.keys():
+        if i<14:
+            joint=parts[i]
+            joint_x=int(joint['x'])
+            joint_y=int(joint['y'])
+            X.append(joint_x)
+            Y.append(joint_y)
+    foot=max(Y)
+    y_trans=base-foot
+    for i in parts.keys():
+        parts[i]['y']=int(parts[i]['y']+y_trans)
+       
+    for pair in connections:
+#        print(pair)
+        start=pair[0]
+        end=pair[1]
+        if start in keys and end in keys:
+            x1=int(parts[start]['x'])
+            x2=int(parts[end]['x'])
+            y1=int(parts[start]['y'])
+            y2=int(parts[end]['y'])
+            cv2.line(img, (x1, y1), (x2, y2), line_color , thickness)
+            
+    if 4 in parts.keys() and 7 in parts.keys():
+        rhand=parts[4]
+        lhand=parts[7]
+
+        cv2.circle(img, (rhand['x'],rhand['y']), gradius , gcolor, -1)
+        cv2.circle(img, (lhand['x'],lhand['y']), gradius , gcolor, -1)
+    
+    if 0 in parts.keys():
+        head=parts[0]
+
+        cv2.circle(img, (head['x'],head['y']), hradius , line_color, -1)
+
+    return img, parts
+
+
+def draw_human(img,parts,h,w,numbering=False):
     connections=[(0,1),(1,2),(1,5),(1,8),(1,11),(2,3),(3,4),(5,6),(6,7),(8,9),(9,10),(11,12),(12,13)]
     keys=list(parts.keys())
 #    print(keys)
@@ -63,8 +223,10 @@ def draw_human(img,parts,scale,numbering=False):
     for i in parts.keys():
         if i<14:
             joint=parts[i]
-            joint_x=int(joint['x'])
-            joint_y=int(joint['y'])
+            joint_x=int(joint['x']*w)
+            joint_y=int(joint['y']*h)
+#            print(joint_x)
+#            print(joint_y)
             cv2.circle(img, (joint_x,joint_y), radius , joint_color, -1)
             if numbering:
                 cv2.putText(img,str(i), (joint_x,joint_y),cv2.FONT_HERSHEY_SIMPLEX, 1,(255,0,0),2)
@@ -75,10 +237,10 @@ def draw_human(img,parts,scale,numbering=False):
         start=pair[0]
         end=pair[1]
         if start in keys and end in keys:
-            x1=int(parts[start]['x'])
-            x2=int(parts[end]['x'])
-            y1=int(parts[start]['y'])
-            y2=int(parts[end]['y'])
+            x1=int(parts[start]['x']*w)
+            x2=int(parts[end]['x']*w)
+            y1=int(parts[start]['y']*h)
+            y2=int(parts[end]['y']*h)
             cv2.line(img, (x1, y1), (x2, y2), line_color , thickness)
 
     return img
@@ -131,12 +293,103 @@ class predictionGrabber(frameGrabber):
                 msg=bytes()
                 for chunk in self.r.iter_content():
                     msg+=chunk
-                    a=msg.find(b'<')
-                    b=msg.find(b'>')
+                    a=msg.find(b'$')
+                    b=msg.find(b'&')
                     if a!=-1 and b!=-1:
                         val=msg[a+1:b]
                         msg=msg[b+1:]
                         self.img=val.decode()
 
+def update_hp(img, score1, score2):
+  
+  max_1 = 200
+  max_2 = 500
+  
+  #player 1 score
+  img = cv2.rectangle(img,(max_1-100,450),(max_1,500),(0,128,0),cv2.FILLED)
+  img = cv2.rectangle(img,(max_1-100+score1,450),(max_1,500),(0,0,255), cv2.FILLED)
+  
+  img = cv2.rectangle(img,(max_2-100,450),(max_2,500),(0,128,0),cv2.FILLED)
+  img = cv2.rectangle(img,(max_2+score2-100,450),(max_2,500),(0,0,255), cv2.FILLED)
+  
+  if score1==100:
+    img = cv2.rectangle(img,(max_1-100,450),(max_1,500),(0,128,0),cv2.FILLED)
+  if score2==100:
+    img = cv2.rectangle(img,(max_2-100,450),(max_2,500),(0,128,0),cv2.FILLED)
+  
+  if score1 <= 0 and score2!=0:
+    msg='PLAYER 2 WINS!!!'
+    return img,msg
+  elif score2 <= 0 and score1!=0:
+    msg='PLAYER 1 WINS!!!'
+    return img,msg
+  elif score1<=0 and score2<=0:
+    msg='DRAW!!!'
+    return img,msg
+  else:
+      return img,0
 
+def start_screen(h,w,xadj,yadj,delay):
+    cute_img = cv2.imread("cute_no_background.png")
+    small = cv2.resize(cute_img, (0,0), fx=0.2, fy=0.2)
+    while True:
+        img = np.zeros((h,w,3), np.uint8)
+        cv2.putText(img, 'Welcome to', (150+xadj, 100+yadj), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA);
+        cv2.putText(img, 'SHADOW FIGHTER!', (30+xadj, 200+yadj), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (0, 0, 255), 5, cv2.LINE_AA);
+        y_offset=230+yadj
+        x_offset=130+xadj
+        img[y_offset:y_offset+small.shape[0], x_offset:x_offset+small.shape[1]] = small
+        cv2.imshow('',img)
+        if cv2.waitKey(1) & 0xff == ord('\r'):
+            print('ahhh')
+            break
+        time.sleep(delay)
+        
+        cv2.putText(img, 'Press enter to start', (100+xadj, 450+yadj), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 0, 255), 2, cv2.LINE_AA);
+        cv2.imshow('',img)
+        if cv2.waitKey(1) & 0xff == ord('\r'):
+            print('ahhh')
+            break
+        time.sleep(delay)
+    
+        
+        img = np.zeros((h,w,3), np.uint8)
+        cv2.putText(img, 'Welcome to', (150+xadj, 100+yadj), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA);
+        cv2.putText(img, 'SHADOW FIGHTER!', (30+xadj, 200+yadj), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (0, 0, 255), 5, cv2.LINE_AA);
+        y_offset=230+yadj
+        x_offset=130+xadj
+        img[y_offset:y_offset+small.shape[0], x_offset:x_offset+small.shape[1]] = small
+        cv2.imshow('',img)
+        if cv2.waitKey(1) & 0xff == ord('\r'):
+            print('ahhh')
+            break
+        time.sleep(delay)
+        
+    
+    
+    
+    img2 = np.zeros((h,w,3), np.uint8)
+    cv2.putText(img2, '3', (160+xadj, 300+yadj), cv2.FONT_HERSHEY_TRIPLEX, 8, (0, 0, 255), 4, cv2.LINE_AA);
+    cv2.imshow('',img2)
+    cv2.waitKey(1)
+    #
+    img2 = np.zeros((h,w,3), np.uint8)
+    cv2.putText(img2, '2', (160+xadj, 300+yadj), cv2.FONT_HERSHEY_TRIPLEX, 8, (0, 0, 255), 4, cv2.LINE_AA);
+    cv2.imshow('',img2)
+    time.sleep(1)
+    cv2.waitKey(1)
+    
+    img2 = np.zeros((h,w,3), np.uint8)
+    cv2.putText(img2, '1', (160+xadj, 300+yadj), cv2.FONT_HERSHEY_TRIPLEX, 8, (0, 0, 255), 4, cv2.LINE_AA);
+    cv2.imshow('',img2)
+    time.sleep(1)
+    cv2.waitKey(1)
+    #
+    img2 = np.zeros((h,w,3), np.uint8)
+    cv2.putText(img2, 'FIGHT!', (40+xadj, 300+yadj), cv2.FONT_HERSHEY_TRIPLEX, 4, (0, 0, 255), 4, cv2.LINE_AA);
+    time.sleep(1)
+    cv2.imshow('',img2)
+    cv2.waitKey(1)
+    time.sleep(1)
+#    cv2.destroyAllWindows()
 
